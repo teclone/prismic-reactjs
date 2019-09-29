e9680a163a85537ef438bc5aa697525999fde6ef
Harrison Ifeanyichukwu <Harrisonifeanyichukwu@gmail.com>
2019-09-29 12:44:20 +0200

    feat: pass down extra props to component

--- src/richtext.js
+++ src/richtext.js
@@ -1,31 +1,51 @@
 import React, { createElement, Fragment } from 'react';
-import PrismicRichText, { Elements } from 'prismic-richtext';
+import PrismicRichText, { Elements } from 'prismic-richtext-jsx';
 import { Link as LinkHelper } from 'prismic-helpers';
 import { embeds } from './embeds';
-const createScript = typeof window !== `undefined` ? require("./embeds").createScript : () => {}
+const createScript = typeof window !== `undefined` ? require('./embeds').createScript : () => {};
 
 function serialize(linkResolver, type, element, content, children, index) {
-  switch(type) {
-    case Elements.heading1: return serializeStandardTag('h1', element, children, index);
-    case Elements.heading2: return serializeStandardTag('h2', element, children, index);
-    case Elements.heading3: return serializeStandardTag('h3', element, children, index);
-    case Elements.heading4: return serializeStandardTag('h4', element, children, index);
-    case Elements.heading5: return serializeStandardTag('h5', element, children, index);
-    case Elements.heading6: return serializeStandardTag('h6', element, children, index);
-    case Elements.paragraph: return serializeStandardTag('p', element, children, index);
-    case Elements.preformatted: return serializeStandardTag('pre', element, children, index);
-    case Elements.strong: return serializeStandardTag('strong', element, children, index);
-    case Elements.em: return serializeStandardTag('em', element, children, index);
-    case Elements.listItem: return serializeStandardTag('li', element, children, index);
-    case Elements.oListItem: return serializeStandardTag('li', element, children, index);
-    case Elements.list: return serializeStandardTag('ul', element, children, index);
-    case Elements.oList: return serializeStandardTag('ol', element, children, index);
-    case Elements.image: return serializeImage(linkResolver, element, index);
-    case Elements.embed: return serializeEmbed(element, index);
-    case Elements.hyperlink: return serializeHyperlink(linkResolver, element, children, index);
-    case Elements.label: return serializeLabel(element, children, index);
-    case Elements.span: return serializeSpan(content);
-    default: return null;
+  switch (type) {
+    case Elements.heading1:
+      return serializeStandardTag('h1', element, children, index);
+    case Elements.heading2:
+      return serializeStandardTag('h2', element, children, index);
+    case Elements.heading3:
+      return serializeStandardTag('h3', element, children, index);
+    case Elements.heading4:
+      return serializeStandardTag('h4', element, children, index);
+    case Elements.heading5:
+      return serializeStandardTag('h5', element, children, index);
+    case Elements.heading6:
+      return serializeStandardTag('h6', element, children, index);
+    case Elements.paragraph:
+      return serializeStandardTag('p', element, children, index);
+    case Elements.preformatted:
+      return serializeStandardTag('pre', element, children, index);
+    case Elements.strong:
+      return serializeStandardTag('strong', element, children, index);
+    case Elements.em:
+      return serializeStandardTag('em', element, children, index);
+    case Elements.listItem:
+      return serializeStandardTag('li', element, children, index);
+    case Elements.oListItem:
+      return serializeStandardTag('li', element, children, index);
+    case Elements.list:
+      return serializeStandardTag('ul', element, children, index);
+    case Elements.oList:
+      return serializeStandardTag('ol', element, children, index);
+    case Elements.image:
+      return serializeImage(linkResolver, element, index);
+    case Elements.embed:
+      return serializeEmbed(element, index);
+    case Elements.hyperlink:
+      return serializeHyperlink(linkResolver, element, children, index);
+    case Elements.label:
+      return serializeLabel(element, children, index);
+    case Elements.span:
+      return serializeSpan(content);
+    default:
+      return null;
   }
 }
 
@@ -34,7 +54,7 @@ function propsWithUniqueKey(props = {}, key) {
 }
 
 function serializeStandardTag(tag, element, children, key) {
-  const props = element.label ? Object.assign({}, {className: element.label}) : {};
+  const props = element.label ? Object.assign({}, { className: element.label }) : {};
   return createElement(tag, propsWithUniqueKey(props, key), children);
 }
 
@@ -52,11 +72,11 @@ function serializeLabel(element, children, key) {
 
 function serializeSpan(content) {
   if (content) {
-    return content.split("\n").reduce((acc, p) => {
+    return content.split('\n').reduce((acc, p) => {
       if (acc.length === 0) {
         return [p];
       } else {
-        const brIndex = ((acc.length + 1) / 2) - 1;
+        const brIndex = (acc.length + 1) / 2 - 1;
         const br = createElement('br', propsWithUniqueKey({}, brIndex));
         return acc.concat([br, p]);
       }
@@ -68,9 +88,9 @@ function serializeSpan(content) {
 
 function serializeImage(linkResolver, element, key) {
   const linkUrl = element.linkTo ? LinkHelper.url(element.linkTo, linkResolver) : null;
-  const linkTarget = (element.linkTo && element.linkTo.target) ? { target: element.linkTo.target } : {};
+  const linkTarget = element.linkTo && element.linkTo.target ? { target: element.linkTo.target } : {};
   const relAttr = linkTarget.target ? { rel: 'noopener' } : {};
-  const img = createElement('img', { src: element.url , alt: element.alt || '' });
+  const img = createElement('img', { src: element.url, alt: element.alt || '' });
 
   return createElement(
     'p',
@@ -84,30 +104,38 @@ function serializeEmbed(element, key) {
     createScript(embeds[element.oembed.provider_name]);
   }
 
-  const className = `embed embed-${element.oembed.provider_name.toLowerCase()}`
-  const props = Object.assign({
-    "data-oembed": element.oembed.embed_url,
-    "data-oembed-type": element.oembed.type,
-    "data-oembed-provider": element.oembed.provider_name,
-    ref: (ref) => {
-      if (embeds[element.oembed.provider_name]) {
-        embeds[element.oembed.provider_name].load(ref)
-      }
+  const className = `embed embed-${element.oembed.provider_name.toLowerCase()}`;
+  const props = Object.assign(
+    {
+      'data-oembed': element.oembed.embed_url,
+      'data-oembed-type': element.oembed.type,
+      'data-oembed-provider': element.oembed.provider_name,
+      ref: ref => {
+        if (embeds[element.oembed.provider_name]) {
+          embeds[element.oembed.provider_name].load(ref);
+        }
+      },
     },
-  }, element.label ? { className: `${className} ${element.label}` } : { className });
+    element.label ? { className: `${className} ${element.label}` } : { className }
+  );
 
-  const embedHtml = createElement('div', { dangerouslySetInnerHTML: { __html: element.oembed.html }});
+  const embedHtml = createElement('div', { dangerouslySetInnerHTML: { __html: element.oembed.html } });
 
   return createElement('div', propsWithUniqueKey(props, key), embedHtml);
 }
 
-export const asText = structuredText => PrismicRichText.asText(structuredText)
+export const asText = structuredText => PrismicRichText.asText(structuredText);
 
-export const renderRichText = (richText, linkResolver, htmlSerializer, Component = Fragment) => {
+export const renderRichText = (richText, linkResolver, htmlSerializer, Component = Fragment, componentProps) => {
   if (Object.prototype.toString.call(richText) !== '[object Array]') {
     console.warn(`Rich text argument should be an Array. Received ${typeof richtext}`);
     return null;
   }
-  const serializedChildren = PrismicRichText.serialize(richText, serialize.bind(null, linkResolver), htmlSerializer);
-  return createElement(Component, propsWithUniqueKey(), serializedChildren);
-}
+  const serializedChildren = PrismicRichText.serialize(
+    richText,
+    serialize.bind(null, linkResolver),
+    htmlSerializer,
+    componentProps
+  );
+  return createElement(Component, componentProps, serializedChildren);
+};
