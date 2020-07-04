4cd294dd2e2b3cac2323f333b13a30102e15a650
Harrison Ifeanyichukwu <Harrisonifeanyichukwu@gmail.com>
2020-07-04 12:53:27 +0200

    fix: render nothing if user html serializer returns undefined

--- src/richtext.js
+++ src/richtext.js
@@ -2,10 +2,95 @@ import { createElement, Fragment } from 'react';
 import { Elements, serialize } from '@teclone/prismic-richtext';
 import { Link as LinkHelper } from 'prismic-helpers';
 import { embeds } from './embeds';
-const createScript =
-  typeof window !== `undefined` ? require('./embeds').createScript : () => {};
 
-function runSerialize(linkResolver, type, element, content, children, index) {
+const createScript =
+  typeof window !== `undefined` ? require('./embeds').createScript : () => { };
+
+const serializeStandardTag = (tag, element, children, key) => {
+  const props = element.label ? Object.assign({}, { className: element.label }) : {};
+  return createElement(tag, propsWithUniqueKey(props, key), children);
+}
+
+const propsWithUniqueKey = (props = {}, key) => {
+  return Object.assign(props, { key });
+}
+
+const serializeHyperlink = (linkResolver, element, children, key) => {
+  const targetAttr = element.data.target ? { target: element.data.target } : {};
+  const relAttr = element.data.target ? { rel: 'noopener' } : {};
+  const props = Object.assign(
+    { href: LinkHelper.url(element.data, linkResolver) },
+    targetAttr,
+    relAttr,
+  );
+  return createElement('a', propsWithUniqueKey(props, key), children);
+}
+
+const serializeLabel = (element, children, key) => {
+  const props = element.data ? Object.assign({}, { className: element.data.label }) : {};
+  return createElement('span', propsWithUniqueKey(props, key), children);
+}
+
+const serializeSpan = (content) => {
+  if (content) {
+    return content.split('\n').reduce((acc, p) => {
+      if (acc.length === 0) {
+        return [p];
+      } else {
+        const brIndex = (acc.length + 1) / 2 - 1;
+        const br = createElement('br', propsWithUniqueKey({}, brIndex));
+        return acc.concat([br, p]);
+      }
+    }, []);
+  } else {
+    return null;
+  }
+}
+
+const serializeImage = (linkResolver, element, key) => {
+  const linkUrl = element.linkTo ? LinkHelper.url(element.linkTo, linkResolver) : null;
+  const linkTarget =
+    element.linkTo && element.linkTo.target ? { target: element.linkTo.target } : {};
+  const relAttr = linkTarget.target ? { rel: 'noopener' } : {};
+  const img = createElement('img', { src: element.url, alt: element.alt || '' });
+
+  return createElement(
+    'p',
+    propsWithUniqueKey({ className: [element.label || '', 'block-img'].join(' ') }, key),
+    linkUrl
+      ? createElement('a', Object.assign({ href: linkUrl }, linkTarget, relAttr), img)
+      : img,
+  );
+}
+
+const serializeEmbed = (element, key) => {
+  if (embeds[element.oembed.provider_name]) {
+    createScript(embeds[element.oembed.provider_name]);
+  }
+
+  const className = `embed embed-${element.oembed.provider_name.toLowerCase()}`;
+  const props = Object.assign(
+    {
+      'data-oembed': element.oembed.embed_url,
+      'data-oembed-type': element.oembed.type,
+      'data-oembed-provider': element.oembed.provider_name,
+      ref: ref => {
+        if (embeds[element.oembed.provider_name]) {
+          embeds[element.oembed.provider_name].load(ref);
+        }
+      },
+    },
+    element.label ? { className: `${className} ${element.label}` } : { className },
+  );
+
+  const embedHtml = createElement('div', {
+    dangerouslySetInnerHTML: { __html: element.oembed.html },
+  });
+
+  return createElement('div', propsWithUniqueKey(props, key), embedHtml);
+}
+
+const runSerialize = (linkResolver, type, element, content, children, index) => {
   switch (type) {
     case Elements.heading1:
       return serializeStandardTag('h1', element, children, index);
@@ -48,90 +133,6 @@ function runSerialize(linkResolver, type, element, content, children, index) {
     default:
       return null;
   }
-}
-
-function propsWithUniqueKey(props = {}, key) {
-  return Object.assign(props, { key });
-}
-
-function serializeStandardTag(tag, element, children, key) {
-  const props = element.label ? Object.assign({}, { className: element.label }) : {};
-  return createElement(tag, propsWithUniqueKey(props, key), children);
-}
-
-function serializeHyperlink(linkResolver, element, children, key) {
-  const targetAttr = element.data.target ? { target: element.data.target } : {};
-  const relAttr = element.data.target ? { rel: 'noopener' } : {};
-  const props = Object.assign(
-    { href: LinkHelper.url(element.data, linkResolver) },
-    targetAttr,
-    relAttr,
-  );
-  return createElement('a', propsWithUniqueKey(props, key), children);
-}
-
-function serializeLabel(element, children, key) {
-  const props = element.data ? Object.assign({}, { className: element.data.label }) : {};
-  return createElement('span', propsWithUniqueKey(props, key), children);
-}
-
-function serializeSpan(content) {
-  if (content) {
-    return content.split('\n').reduce((acc, p) => {
-      if (acc.length === 0) {
-        return [p];
-      } else {
-        const brIndex = (acc.length + 1) / 2 - 1;
-        const br = createElement('br', propsWithUniqueKey({}, brIndex));
-        return acc.concat([br, p]);
-      }
-    }, []);
-  } else {
-    return null;
-  }
-}
-
-function serializeImage(linkResolver, element, key) {
-  const linkUrl = element.linkTo ? LinkHelper.url(element.linkTo, linkResolver) : null;
-  const linkTarget =
-    element.linkTo && element.linkTo.target ? { target: element.linkTo.target } : {};
-  const relAttr = linkTarget.target ? { rel: 'noopener' } : {};
-  const img = createElement('img', { src: element.url, alt: element.alt || '' });
-
-  return createElement(
-    'p',
-    propsWithUniqueKey({ className: [element.label || '', 'block-img'].join(' ') }, key),
-    linkUrl
-      ? createElement('a', Object.assign({ href: linkUrl }, linkTarget, relAttr), img)
-      : img,
-  );
-}
-
-function serializeEmbed(element, key) {
-  if (embeds[element.oembed.provider_name]) {
-    createScript(embeds[element.oembed.provider_name]);
-  }
-
-  const className = `embed embed-${element.oembed.provider_name.toLowerCase()}`;
-  const props = Object.assign(
-    {
-      'data-oembed': element.oembed.embed_url,
-      'data-oembed-type': element.oembed.type,
-      'data-oembed-provider': element.oembed.provider_name,
-      ref: ref => {
-        if (embeds[element.oembed.provider_name]) {
-          embeds[element.oembed.provider_name].load(ref);
-        }
-      },
-    },
-    element.label ? { className: `${className} ${element.label}` } : { className },
-  );
-
-  const embedHtml = createElement('div', {
-    dangerouslySetInnerHTML: { __html: element.oembed.html },
-  });
-
-  return createElement('div', propsWithUniqueKey(props, key), embedHtml);
 }
 
 export const renderRichText = (
